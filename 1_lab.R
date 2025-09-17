427c4e8e70a4024f70e74d0969890c2275a20dd0
Darj0 <baranovadar04@gmail.com>
2025-09-17 14:06:28 +0300

    Update 1_lab.R

--- 1_lab.R
+++ 1_lab.R
@@ -19,33 +19,11 @@ data$Gender_num <- ifelse(data$Gender == "Female", 1, 0)  # kiekybinė lytis kor
 # ------------------- 2. Pradinė kintamojo "Salary" analizė -------------------
 summary(data$Salary)
 
-hist(data$Salary,
-     breaks = 30,
-     main = "Pradinis atlyginimo pasiskirstymas",
-     xlab = "Atlyginimas",
-     col = "lightblue",
-     border = "white")
-
-# Kvadratinė šaknis ir logaritminė transformacija
-data$Salary_sqrt <- sqrt(data$Salary)
-data$Salary_log <- log(data$Salary)
 
 # ------------------- 3. Normalumo testai -------------------
 # Kolmogorovo–Smirnovo testas
 ks.test(data$Salary, "pnorm", mean = mean(data$Salary), sd = sd(data$Salary))
 
-# Shapiro-Wilk testas
-shapiro.test(data$Salary)
-
-# Anderson-Darling testas
-ad.test(data$Salary)
-ad.test(data$Salary_sqrt)
-ad.test(data$Salary_log)
-
-# QQ grafikas
-qqPlot(data$Salary, dist = "norm",
-       main = "QQ grafikas: atlyginimas",
-       xlab = "Teoriniai kvantiliai", ylab = "Stebėti atlyginimai")
 
 # Histogramos su tankio linija
 options(scipen = 999)
@@ -54,12 +32,20 @@ hist(data$Salary, breaks=30, freq=FALSE, col="darkgray",
 x_vals <- seq(min(data$Salary), max(data$Salary), length.out=100)
 lines(x_vals, dnorm(x_vals, mean=mean(data$Salary), sd=sd(data$Salary)), col="red", lwd=2)
 
+
+# QQ grafikas
+qqPlot(data$Salary, dist = "norm",
+       main = "QQ grafikas: atlyginimas",
+       xlab = "Teoriniai kvantiliai", ylab = "Stebėti atlyginimai")
+
 # ------------------- 4. Koreliacija (kiekybiniai kintamieji) -------------------
 num_vars <- data[, c("Salary", "Experience", "Age", "Gender_num")]
 cor_matrix <- cor(num_vars, use="complete.obs")
 print(cor_matrix)
 corrplot(cor_matrix, method="number", type="upper")
 
+
+### prideti issilavinima bei pareigas
 # ------------------- 5. Stačiakampės diagramos -------------------
 mano_tema <- theme_minimal(base_size=14) +
   theme(
@@ -109,28 +95,3 @@ plot(data$Age, data$Salary,
 abline(lm(Salary ~ Age, data=data), col="red", lwd=2)
 legend("topleft", legend=paste("Koreliacija:", round(cor(data$Age, data$Salary),2)), bty="n")
 
-# ------------------- 7. Pilnas regresijos modelis -------------------
-model_full <- lm(Salary ~ Education + Experience + Location + Job_Title + Age + Gender, data=data)
-summary(model_full)
-
-# Diagnostikos grafikai
-par(mfrow=c(2,2))
-plot(model_full)
-
-# ------------------- 8. Kintamųjų atranka pagal AIC -------------------
-model_step <- stepAIC(model_full, direction="both", trace=FALSE)
-summary(model_step)
-
-par(mfrow=c(2,2))
-plot(model_step)
-
-# ------------------- 9. Prognozės pavyzdys -------------------
-new_data <- data.frame(
-  Education="High School",
-  Experience=5,
-  Location="Rural",
-  Job_Title="Analyst",
-  Age=28,
-  Gender="Female"
-)
-predict(model_step, newdata=new_data, interval="prediction")
